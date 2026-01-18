9104e22c30a0cbcdd429cc295b96a1dc8309f32f
AshtinDonuts <kingwonghku@gmail.com>
2026-01-18 11:13:08 +1030

    For 3d detector separated into node and functionality

---
+++ src/rover_internal/mapping_module/mapping_module/nodes/__init__.py
@@ -0,0 +1,9 @@
+"""
+Nodes package for mapping_module
+
+This package contains ROS2 node implementations.
+"""
+
+from mapping_module.nodes.zed_3d_detector_node import ZED3DDetector
+
+__all__ = ['ZED3DDetector']
